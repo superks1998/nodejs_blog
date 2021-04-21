baea877563af229d4f869af52e06713c3a2008ba
superks1998 <superks1998@gmail.com>
2021-04-21 22:41:14 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// Public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP Logger
+app.use(morgan('combined'));
+
+// Template engine handlebars
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
